f9c0b0d5284db6bd9d6843b12ae162031ea1822a
luandevpro <tranluan1c@gmail.com>
2018-08-29 21:39:14 +0700

    nextjs start kit small

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Head from "../components/head";
+import Nav from "../components/nav";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default () => (
+  <div>
+    <Head title="Home" />
+    <Nav />
+   </div>
+);
+
